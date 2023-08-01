1f03e8f28da5122b01447179faa09dd68f4ec9c9
NGoldy <beatup.n@gmail.com>
2023-08-01 19:35:30 +0300

    сума всіх елементів списку

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-
+# створити список
 mylist = [1, 3, 5, 4]
-print(mylist)
+# сума всіх елементів списку
+print("сума елементів")
 s= mylist[0] + mylist[1] + mylist[2] + mylist[3]
 print(s)
+# список, в якому деякі елементи повторюються
+replist = [2, 5, 5, 4, 2, 3]
+unique_replist = list(set(replist))
+print('унікальні значення')
+print(unique_replist)
+print('сума унікальних значень списку')
+print(sum(unique_replist))
+
+
+
 
 
 
