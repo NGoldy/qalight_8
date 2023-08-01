117b998b8def67ba40e927eb85e31972642a0c47
NGoldy <beatup.n@gmail.com>
2023-08-01 18:51:54 +0300

    сума всіх елементів списку

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+mylist = [1, 3, 5, 4]
+print(mylist)
+s= mylist[0] + mylist[1] + mylist[2] + mylist[3]
+print(s)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("hallo")
+
