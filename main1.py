4d28cb6d408ed2b84585df9d665379a0b71d620c
NGoldy <beatup.n@gmail.com>
2023-08-09 22:57:43 +0300

    Lesson 4 (1) Class Human

--- main1.py
+++ main1.py
@@ -1,10 +1,22 @@
-class Animal:
-    age = 10
-    name = 'kandy'
+class Human:
+    favorite_drink = 'beer'
+    def __init__(self, age: int):
+        self.age = age
+        if self.age < 18:
+            self.favorite_drink = 'juce'
 
-        def __init__(self, age, name):
-           self.age = age
-           self.name = name
+    def drink(self):
+        class_name = self.__class__.__name__
+        fav_drink = self.favorite_drink
+        print(f' {class_name} likes drink {fav_drink}')
+
+man1 = Human(17)
+man2 = Human(28)
+
+man1.drink()
+man2.drink()
+
+
 
 
 
