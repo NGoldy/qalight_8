f77fd62e34311028d0fc2eb416215eaf989e762b
NGoldy <beatup.n@gmail.com>
2023-08-11 21:08:03 +0300

    Lesson 4 (2) Class Human worker

--- main1.py
+++ main1.py
@@ -8,10 +8,15 @@ class Human:
     def drink(self):
         class_name = self.__class__.__name__
         fav_drink = self.favorite_drink
-        print(f' {class_name} likes drink {fav_drink}')
+        print(f' {class_name} likes drink {fav_drink}. He is {self.age} years old.')
 
 class Worker(Human):
-    pass
+
+    def __init__(self, age, salary):
+        super().__init__(age)
+        self.salary = salary
+        if self.age > 18 and self.salary > 1000:
+            self.favorite_drink = 'wiskey'
 
 
 man1 = Human(17)
@@ -20,8 +25,12 @@ man2 = Human(28)
 man1.drink()
 man2.drink()
 
-w1 = Worker(15)
+w1 = Worker(15, 1000)
 w1.drink()
+w2 = Worker(43, 1000)
+w2.drink()
+w3 = Worker(37, 2000)
+w3.drink()
 
 
 
