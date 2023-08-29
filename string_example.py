89b46b5168c6c783ec1bd15aad832d04a29ab5f8
NGoldy <beatup.n@gmail.com>
2023-08-30 01:04:05 +0300

    Lesson Strings

--- string_example.py
+++ string_example.py
@@ -7,5 +7,17 @@ r3 = 'Кількість слів у рядку:'
 print(r3)
 r1.split(' ')
 print(len(r1.split(' ')))
+print('Замінити всі голосні на #:')
+r4 = r1.replace('у', '#')
+r5 = r4.replace('е', '#')
+r6 = r5.replace('і', '#')
+r7 = r6.replace('а', '#')
+r8 = r7.replace('о', '#')
+r9 = r8.replace('є', '#')
+r10 = r9.replace('я', '#')
+r11 = r10.replace('и', '#')
+r12 = r11.replace('ю', '#')
+print(r4)
+
 
 
